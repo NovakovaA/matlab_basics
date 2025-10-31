efcd948b54875b191fbe28150d7626656bb8af77
Anett <126421398+NovakovaA@users.noreply.github.com>
2025-10-31 12:19:17 +0100

    Update helix.m

--- helix.m
+++ helix.m
@@ -18,4 +18,24 @@ for i = 1:m
     else
         plot3(x(idxst:idxe),y(idxst:idxe),z(idxst:idxe),"bo");
     end
+
+end
+
+
+clc,clear, close all
+R=3;
+M=3;
+H=3;
+z=0:(2*pi)/50:(2*pi)-2*pi/360;
+x=sin(z)*R;
+y=cos(z)*R;
+for i=0:M-1
+    if mod(i,2)==0
+        plot3(x,y,((z/(2*pi)+i)/M)*H,'xb',markersize=8)
+        hold on
+        grid on
+        axis equal
+    else
+        plot3(x,y,((z/(2*pi)+i)/M)*H,'.r',markersize=14)
+    end
 end
